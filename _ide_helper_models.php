366abd2f8d22aef8a37a5d3064d99370b525e3f6
Fakhry <fakhry.linux@gmail.com>
2023-08-11 01:37:02 +0800

    Import Data Seed Menggunakan File JSON
    Install Laravel Ide helper
    composer require --dev barryvdh/laravel-ide-helper
    php artisan ide-helper:generate
    php artisan ide-helper:models
    php artisan ide-helper:meta
    re-Generate db
    php artisan migrate:fresh --seed

---
+++ _ide_helper_models.php
@@ -0,0 +1,106 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\City
+ *
+ * @property int $id
+ * @property int $code
+ * @property string $title
+ * @property int $province_code
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|City newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|City newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|City query()
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereProvinceCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereUpdatedAt($value)
+ */
+	class City extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Courier
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Courier newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Courier newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Courier query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Courier whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Courier whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Courier whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Courier whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Courier whereUpdatedAt($value)
+ */
+	class Courier extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Province
+ *
+ * @property int $id
+ * @property int $code
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Province newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Province newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Province query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Province whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Province whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Province whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Province whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Province whereUpdatedAt($value)
+ */
+	class Province extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
